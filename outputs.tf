6f8753358cba8ba664b95a7a66ec31dd61dadf3d
Tomas Bily <tomas.bily@tbily.cz>
2024-10-01 11:44:14 +0200

    add web ID in output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "web1_name" {
- value       = digitalocean_droplet.web1.name
+output "web_name" {
+ value       = digitalocean_droplet.web.name
  description = "Name of the web app droplet"
+}
+output "web_id" {
+ value       = digitalocean_droplet.web.id
+ description = "ID of the web app droplet"
 }
